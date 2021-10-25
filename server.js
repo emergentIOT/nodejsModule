a62b7d152238008b09f118fbc780daf1719a8a2a
Amandeep Singh <Amandeep.Singh4@transport.nsw.gov.au>
2021-10-25 13:37:46 +1100

    Updated bootstrap slider

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const url = require('url');
 
 app.use('/AIMS_DATA2', express.static(path.resolve(__dirname, 'AIMS_DATA2')));
 app.set('view engine', 'ejs');
-
 app.listen(port, () => {console.log(`Server up at PORT: ${port}`);})
-
 
 
 app.get("/", function(req, res) {
